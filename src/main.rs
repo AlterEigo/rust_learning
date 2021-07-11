ebfd58ece5e0b0178e2e5619fc75465cb0fc604e
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2021-07-11 17:17:14 +0300

    Add: multistring literal and warning suppression.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+#![allow(warnings)]
+
+const HELP_MESSAGE:&str = "
+USAGE:
+
+    No options nor arguments yet.
+";
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", HELP_MESSAGE);
 }
 
 fn add(lhs: f64, rhs: f64) -> f64
